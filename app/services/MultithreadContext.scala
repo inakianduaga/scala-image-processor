e5412c4c310313ab3dfc5e087591c05a0d415e43
Inaki Anduaga <inaki.anduaga@tado.com>
2016-03-09 01:00:54 +0100

    #4: Some cleaning up

--- app/services/MultithreadContext.scala
+++ app/services/MultithreadContext.scala
@@ -1,7 +1,7 @@
 package services
+
 import play.api.Play.current
 import play.api.libs.concurrent.Akka
-
 import scala.concurrent.ExecutionContext
 
 object MultithreadContext {
@@ -9,6 +9,5 @@ object MultithreadContext {
   /**
    * https://www.playframework.com/documentation/2.4.x/ThreadPools#Using-other-thread-pools
    */
-//  implicit val singleThreadExecutionContext: ExecutionContext = Akka.system.dispatchers.lookup("single-thread-context")
   implicit val multiThreadExecutionContext: ExecutionContext = Akka.system.dispatchers.lookup("multi-thread-context")
 }
